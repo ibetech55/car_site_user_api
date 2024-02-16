038559ea93e7642c16a07f1d3c9e91da57619308
pibewiro <pibewiro@yahoo.com>
2024-02-15 22:53:43 -0300

    feat: integrate change password

--- src/Containers/Users/ChangePassword/index.ts
+++ src/Containers/Users/ChangePassword/index.ts
@@ -1,11 +1,12 @@
 import { ChangeUserPasswordController } from "../../../Controllers/UserControllers/ChangePasswordController";
 import { ChangeUserPasswordUseCase } from "../../../Presentation/Users/ChangeUserPasswordUseCase";
+import { rabbitMq } from "../../../Queue/RabbitMq";
 import { UserRepository } from "../../../Repositories/User/user.repository";
 import { GeneratePassword } from "../../../Utils/GeneratePassword";
 
 const userRepository = new UserRepository();
-const generatePassword = new GeneratePassword()
-const changeUserPasswordUseCase = new ChangeUserPasswordUseCase(userRepository, generatePassword)
+const generatePassword = new GeneratePassword();
+const changeUserPasswordUseCase = new ChangeUserPasswordUseCase(userRepository, generatePassword, rabbitMq)
 const changeUserPasswordController = new ChangeUserPasswordController(changeUserPasswordUseCase)
 
 export {changeUserPasswordUseCase, changeUserPasswordController}
--- src/Presentation/Users/ChangeUserPasswordUseCase/index.ts
+++ src/Presentation/Users/ChangeUserPasswordUseCase/index.ts
@@ -1,30 +1,53 @@
 import { AppError } from "../../../ErrorHandler/AppError";
+import { UpdateAuthUserProviderDto } from "../../../Queue/QueueDtos/UpdateAuthUserProviderDto";
+import { RabbitMq } from "../../../Queue/RabbitMq/RabbitMq";
+import { USER_API_UPDATE_AUTH_USER } from "../../../Queue/types";
 import { IUserRepository } from "../../../Repositories/User/IUserRespository";
 import { GeneratePassword } from "../../../Utils/GeneratePassword";
 
 export class ChangeUserPasswordUseCase {
-  private _repository: IUserRepository;
-  private _generatePassword:GeneratePassword;
+  private readonly _repository: IUserRepository;
+  private readonly _generatePassword: GeneratePassword;
+  private readonly _rabbitMq: RabbitMq;
 
-  constructor(repository: IUserRepository, generatePassword:GeneratePassword) {
+  constructor(repository: IUserRepository, generatePassword: GeneratePassword, rabbitMq: RabbitMq) {
     this._repository = repository;
     this._generatePassword = generatePassword;
+    this._rabbitMq = rabbitMq;
   }
 
-  async execute(id: string, oldPassword:string, newPassword: string): Promise<Boolean> {
+  async execute(
+    id: string,
+    oldPassword: string,
+    newPassword: string
+  ): Promise<Boolean> {
     const userData = await this._repository.getUserById(id);
     if (!userData) {
       throw new AppError("No user found", 404);
     }
 
-    const passwordDecoded = this._generatePassword.decryptPassword(oldPassword, userData.password)
+    const passwordDecoded = this._generatePassword.decryptPassword(
+      oldPassword,
+      userData.password
+    );
     if (!passwordDecoded) {
       throw new AppError("Incorrect password, please try again", 400);
     }
 
-    const passwordEncoded = this._generatePassword.encryptPassword(newPassword)
+    const passwordEncoded = this._generatePassword.encryptPassword(newPassword);
 
-    const data = await this._repository.updateUser(id, { password: passwordEncoded });
+    const data = await this._repository.updateUser(id, {
+      password: passwordEncoded,
+    });
+    const userDataUpdated = await this._repository.getUserById(id);
+
+    this._rabbitMq.publish<UpdateAuthUserProviderDto>(
+      USER_API_UPDATE_AUTH_USER,
+      {
+        user_id: userDataUpdated._id,
+        password: userDataUpdated.password,
+      }
+    );
     return data;
   }
 }
